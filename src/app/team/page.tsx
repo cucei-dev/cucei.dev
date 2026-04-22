bf8de5d13875ee0e8767716cc55c0bf0bc60c2bd
milosnowcat <ramiro.alvarez@rahcode.com>
2026-04-22 13:38:00 -0600

    feat: Tean page

---
+++ src/app/team/page.tsx
@@ -0,0 +1,14 @@
+export default function TeamPage() {
+  return (
+    <section className="min-h-screen flex items-center pt-20">
+      <div className="max-w-7xl mx-auto px-6 py-20">
+        <h1 className="text-5xl md:text-7xl font-black mb-8 leading-[0.9] tracking-tighter uppercase">
+          Nuestro <span className="text-primary">Equipo</span>
+        </h1>
+        <p className="md:text-xl text-white font-light mb-12 max-w-2xl leading-relaxed">
+          Próximamente conocerás al equipo detrás de CUCEI.DEV
+        </p>
+      </div>
+    </section>
+  );
+}
